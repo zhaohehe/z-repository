e5a2e40db55823e6ab7defdc7ad41d86ba27cb84
zhaohehe <zhaohehedola@gmail.com>
2016-12-22 15:34:20 +0800

    完成criteria

---
+++ src/Zhaohehe/Repositories/Criteria/Criteria.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * Sometime too hot the eye of heaven shines
+ */
+
+namespace Zhaohehe\Repositories\Criteria;
+
+
+/**
+ * Class Criteria
+ */
+abstract class Criteria
+{
+    /**
+     * @param $model
+     * @param $repository
+     *
+     * @return mixed
+     */
+    public abstract function apply($model, $repository);
+
+}
--- src/Zhaohehe/Repositories/Eloquent/Repository.php
+++ src/Zhaohehe/Repositories/Eloquent/Repository.php
@@ -5,8 +5,10 @@ namespace Zhaohehe\Repositories\Eloquent;
 use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Container\Container as App;
+use Zhaohehe\Repositories\Criteria\Criteria;
 use Zhaohehe\Repositories\Contracts\RepositoryInterface;
 use Zhaohehe\Repositories\Exceptions\RepositoryException;
+
 
 
 /**
@@ -210,11 +212,66 @@ abstract class Repository implements RepositoryInterface
     }
 
 
+
+    /**
+     * @return Collection
+     */
+    public function getCriteria()
+    {
+        return $this->criteria;
+    }
+
+
+    /**
+     * @param Criteria $criteria
+     *
+     * @return $this
+     */
+    public function getByCriteria(Criteria $criteria)
+    {
+        $this->model = $criteria->apply($this->model, $this);
+
+        return $this;
+    }
+
+
+    /**
+     * @param Criteria $criteria
+     *
+     * @return $this
+     */
+    public function pushCriteria(Criteria $criteria)
+    {
+        //remove if criteria existed
+        $key = $this->criteria->search(function ($item) use ($criteria) {
+            return is_object($item) && (get_class($item) == get_class($criteria));
+        });
+        if (is_int($key)) {
+            $this->criteria->offsetUnset($key);
+        }
+
+        $this->criteria->push($criteria);
+        return $this;
+    }
+
+
+    /**
+     * apply Criteria
+     *
+     * @return $this
+     */
     public function applyCriteria()
     {
         if ($this->skipCriteria == true) {
             return $this;
         }
 
+        foreach ($this->getCriteria() as $criteria) {
+            if ($criteria instanceof Criteria) {
+                $this->model = $criteria->apply($this->model, $this);
+            }
+        }
+
+        return $this;
     }
 }
